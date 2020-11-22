6b2ac7b715e4fb69603a039067f64a5e78b778d6
Eduarda Taschetto <eduarda.b.taschetti@gmail.com>
2020-11-21 22:01:04 -0300

    commit

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-/* menu toggle */
+const currentPage = location.pathname
+const menuItems = document.querySelectorAll("ul li a")
 
+for (item of menuItems) {
+    if (currentPage.includes(item.getAttribute("href"))) {
+        item.classList.add("active")
+    }
+}
+
+
+
+
+/* menu toggle */
 let show = true;
 
 const menuSection = document.querySelector(".menu-section")
@@ -9,4 +20,20 @@ menuToggle.addEventListener('click', () => {
     document.body.style.overflow = show ? "hidden" : "initial"
     menuSection.classList.toggle('on', show)
     show = !show
+})
+
+menuToggle.addEventListener('mouseover', () => {
+    let div = menuToggle.querySelectorAll("div")
+
+    for (let i = 0; i < div.length; i++) {
+        div[i].style.backgroundColor = "#31a24c"
+    }
+})
+
+menuToggle.addEventListener('mouseout', () => {
+    let div = menuToggle.querySelectorAll("div")
+
+    for (let i = 0; i < div.length; i++) {
+        div[i].style.backgroundColor = "#555"
+    }
 })
