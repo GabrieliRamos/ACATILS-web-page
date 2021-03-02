5ee031d7f96142c70c6e27d02efa0a724ab5b12e
Eduarda Taschetto <eduarda.b.taschetti@gmail.com>
2021-03-02 20:27:15 -0300

    deploy e ajustes

--- acatils/core/static/js/translate.js
+++ acatils/core/static/js/translate.js
@@ -4,11 +4,11 @@ function ShowIframe() {
 
     if (iframe.style.display != 'flex') {
         iframe.style.display = 'flex'
-        img.src = '/static/img/close-icon.png'
         img.style.filter = 'invert(100%)'
+        img.src = '/static/img/close-icon.png'
     } else {
         iframe.style.display = 'none'
-        img.src = '/static/img/libras-icon.png'
         img.style.filter = 'invert(0%)'
+        img.src = '/static/img/libras-icon.png'
     }
 }
